d13b247e35eb158f34ca1ea764a8d8ae66267314
Taylor Wise <taylor2julie@gmail.com>
2020-04-16 23:37:58 -0500

    I believe I have completed MVP however I am still working on the project.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import "./App.css";
 import PhotoCard from "./components/PhotoCard";
+import styled from "styled-components";
+
+const AppBG = styled.div`
+font-family: 'Concert One', cursive;
+font-size: 1.8rem;
+display: flex;
+flex-direction: column;
+align-items: center;
+min-height: 100vh;
+background-image: linear-gradient(to bottom right, #04032b, #6c3990, #84037c);
+`;
+
+const NasaTitle = styled.h1`
+color: #3366ff;
+text-shadow: 3px 3px white;
+`;
 
 const App = () => {
 
@@ -10,19 +26,17 @@ const App = () => {
   useEffect(() => {
     axios
     .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-    .then(response =>  setPhoto([response.data]))
+    .then(response =>  setPhoto(response.data))
     .catch(error => console.log("Error!", error))
   }, [])
 
   console.log(photo)
 
   return (
-    <div className="App">
-      <h1>Nasa's Astronomy Photo Of The Day!</h1>
-      {photo.map(image => (
-        <PhotoCard key={image.id} image={image}/>
-      ))}
-    </div>
+    <AppBG>
+      <NasaTitle>NASA Astronomy Photo Of The Day!</NasaTitle>
+      <PhotoCard image={photo}/>
+    </AppBG>
   );
 };
 
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,17 +1,53 @@
 import React from "react";
 import "../App";
 import "./PhotoCard.css";
+import { CardBody, CardTitle, CardSubtitle
+  } from 'reactstrap';
+import styled from "styled-components";
 
-const PhotoCard = (props) => {
-    const {image} = props;
+const CardWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;'
+`;
+
+const  PhotoTitle = styled.div`
+display: flex;
+justify-content: space-between;
+color: white;
+`;
+
+const AstroImg = styled.img`
+  border-radius: 10%;
+  border: 8px double #0b3d91;
+`;
+
+const InfoWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
+const CardInformation = styled.div`
+width: 55%;
+text-align: center;
+color: white;
+`;
+
+
+
+const PhotoCard = ({image}) => {
     return(
-        <div className="photo-card">
-            <h3>{image.title}</h3>
-            <p>Date: {image.date}</p>
-            <img className="photo" alt="Nasa's photo of the day." src={image.url} /> 
-            <p className="photo-description">{image.explanation }</p>
-        </div>
+        <CardWrapper>
+        <CardBody>
+          <CardTitle>{image.title}</CardTitle>
+          <CardSubtitle>{image.date}</CardSubtitle>
+        </CardBody>
+        <AstroImg width="55%" src={image.url} alt="Nasa's astronomy photo of the day." />
+        <InfoWrapper>
+          <CardInformation>{image.explanation}</CardInformation>
+        </InfoWrapper>
+        </CardWrapper>
     );
 };
-
 export default PhotoCard;
